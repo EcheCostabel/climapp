67a5a67eab17bf5e1f53b5fe11771f0ae7d911ff
EcheCostabel <exequielcostabel@gmail.com>
2023-02-22 19:22:41 -0300

    change img

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,12 +9,16 @@ export const Card = ({loadingData, showData, weather, forecast}) => {
     var year = today.getFullYear();
     var date = day + '/' + month + '/' + year;
 
+    var url = '';
+    var iconUrl = '';
+
     if(loadingData){
         return <Spinner />
     }
 
     if(showData){
-        
+        url='http://openweathermap.org/img/w/';
+        iconUrl = url + weather.weather[0].icon + '.png'
     }
 
   return (
@@ -28,7 +32,8 @@ export const Card = ({loadingData, showData, weather, forecast}) => {
                                 <h3 className='card-title'>{weather.name}</h3>
                                 <p className='card-date'>{date}</p>
                                 <h1 className='card-temp'>{(weather.main.temp - 273.15).toFixed(1)}°C</h1>
-                                <img  src='https://images.pexels.com/photos/2097616/pexels-photo-2097616.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                                <p className='card-desc'><img src={iconUrl} alt='icon'/>{weather.weather[0].description}</p>
+                                <img  src='https://images.pexels.com/photos/2129796/pexels-photo-2129796.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
                                 className='img-fluid rounded-start'
                                 alt='...'
                                 />
