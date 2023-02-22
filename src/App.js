9bdc0fb0306ac360047bb2524a31066342e30945
EcheCostabel <exequielcostabel@gmail.com>
2023-02-22 11:55:03 -0300

    adding form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './assets/CSS/App.css';
+import { Form } from './assets/CSS/components/Form';
 import { NavBar } from './assets/CSS/components/NavBar';
 
 
 function App() {
   return (
     <div className="App">
-      <NavBar  />
+      <NavBar  /> 
+      <Form />
     </div>
   );
 }
