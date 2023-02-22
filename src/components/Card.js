f98d4763a966b9693a13c6d78e26a6403fb03ea0
EcheCostabel <exequielcostabel@gmail.com>
2023-02-22 20:16:29 -0300

    finish day

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,6 +12,14 @@ export const Card = ({loadingData, showData, weather, forecast}) => {
     var url = '';
     var iconUrl = '';
 
+    var iconUrl3 = '';
+    var iconUrl6 = '';
+    var iconUrl9 = '';
+
+    var forecastDate3 = '';
+    var forecastDate6 = '';
+    var forecastDate9 = '';
+
     if(loadingData){
         return <Spinner />
     }
@@ -19,6 +27,14 @@ export const Card = ({loadingData, showData, weather, forecast}) => {
     if(showData){
         url='http://openweathermap.org/img/w/';
         iconUrl = url + weather.weather[0].icon + '.png'
+
+        iconUrl3 = url + forecast.list[1].weather[0].icon + '.png'
+        iconUrl6 = url + forecast.list[2].weather[0].icon + '.png'
+        iconUrl9 = url + forecast.list[3].weather[0].icon + '.png'
+
+        forecastDate3 = forecast.list[1].dt_txt.substring(11, 13); // CON ESTO OBTENGO SOLO LA HORA
+        forecastDate6 = forecast.list[2].dt_txt.substring(11, 13); // CON ESTO OBTENGO SOLO LA HORA
+        forecastDate9 = forecast.list[3].dt_txt.substring(11, 13); // CON ESTO OBTENGO SOLO LA HORA
     }
 
   return (
@@ -44,8 +60,28 @@ export const Card = ({loadingData, showData, weather, forecast}) => {
                                     <h5 className='card-text'>Temperatura mínima: {(weather.main.temp_min - 275.15).toFixed(1)}°C</h5>
                                     <h5 className='card-text'>Sensación térmica: {(weather.main.feels_like - 275.15).toFixed(1)}°C</h5>
                                     <h5 className='card-text'>Humedad: {(weather.main.humidity)}%</h5>
-                                    <h5 className='card-text'>Velocidad del viento: {(weather.main.wind.speed)}m/s</h5>
+                                    <h5 className='card-text'>Velocidad del viento: {(weather.wind.speed)}m/s</h5>
                                 </div>
+                                <hr />
+
+                                <div className='row mt-4'>
+                                    <div className='col'>
+                                        <p>{forecastDate3}h</p>
+                                        <p className='description'><img src={iconUrl3} alt='...' />{forecast.list[1].weather[0].description}</p>
+                                        <p className='temp'>{(forecast.list[1].main.temp - 273.15).toFixed(1)}°C</p>
+                                    </div>
+                                    <div className='col'>
+                                        <p>{forecastDate6}h</p>
+                                        <p className='description'><img src={iconUrl6} alt='...' />{forecast.list[2].weather[0].description}</p>
+                                        <p className='temp'>{(forecast.list[2].main.temp - 273.15).toFixed(1)}°C</p>
+                                    </div>
+                                    <div className='col'>
+                                        <p>{forecastDate9}h</p>
+                                        <p className='description'><img src={iconUrl9} alt='...' />{forecast.list[3].weather[0].description}</p>
+                                        <p className='temp'>{(forecast.list[3].main.temp - 273.15).toFixed(1)}°C</p>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                     </div>
