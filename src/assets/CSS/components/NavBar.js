0ef57b5355dbc5416aa244aba517eeaa5425929b
EcheCostabel <exequielcostabel@gmail.com>
2023-02-22 11:28:22 -0300

    starting day with rain and react

---
+++ src/assets/CSS/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <nav className='navbar bg-dark text-light mb-5'>
+        <div className='container-fluid'>
+            <h3 className='mx-auto'>Predicción Metereológica</h3>
+        </div>
+    </nav>
+  )
+}
